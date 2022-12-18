b97357650abb5af585584ad6e4c3465afb16177c
Christina Yanovich <christik.lucid@gmail.com>
2022-12-18 13:51:51 +0300

    style: настраивает линтер и подключает airbnb style guide

--- src/index.jsx
+++ src/index.jsx
@@ -5,11 +5,10 @@ import ReactDOM from 'react-dom/client';
 
 import App from './components/app/app';
 
-
 const root = ReactDOM.createRoot(document.getElementById('app'));
 
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
