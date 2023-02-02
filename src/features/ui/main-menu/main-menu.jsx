c7d19bd4cbc127bbe8454a748062ab09850dcf11
Christik <christik.lucid@gmail.com>
2023-02-02 10:28:26 +0300

    feat: добавляет ссылкам меню состояние при наведении

--- src/features/ui/main-menu/main-menu.jsx
+++ src/features/ui/main-menu/main-menu.jsx
@@ -1,6 +1,6 @@
 import './main-menu.scss';
 
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import classnames from 'classnames';
 
 import { AppRoute } from 'constants';
@@ -9,13 +9,14 @@ function MainMenu({ className }) {
   return (
     <ul className={classnames('jd-main-menu', className)}>
       <li className="jd-main-menu__item">
-        <Link
-          className="jd-main-menu__link"
+        <NavLink
+          className={({ isActive }) => (isActive ? 'jd-main-menu__link jd-main-menu__link--active' : 'jd-main-menu__link')}
           to={AppRoute.CATALOG}
+          end
         >
           <span className="jd-icon jd-icon--check-circle jd-main-menu__link__icon" />
           <span className="jd-main-menu__link__text">Задания</span>
-        </Link>
+        </NavLink>
       </li>
 
       <li className="jd-main-menu__item">
